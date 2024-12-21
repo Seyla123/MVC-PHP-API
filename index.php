be69947de29d72557f8843252318d6367b66fc54
Seav Seyla <Mrrseyla.758@gmail.com>
2024-12-21 10:20:18 +0700

    feat(The Front Controller) : Add Another Controller and Select it in the Front Controller

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <?php 
-    require "src/controllers/products.php";
-    $controller = new Products();
 
-    $action = $GET["action"]??"index";
+    $action = $_GET["action"]??"index";
+    $controller = $_GET["controller"]??"products";	
 
-    if($action == 'show') {
-        $controller->show();
-    }else{
-        $controller->index();
+    if($controller === 'products') {
+        require "src/controllers/products.php";
+        $controller_object = new Products();
+    }elseif ($controller === "home") {
+        require "src/controllers/home.php";
+        $controller_object = new Home();
+    }
+    if($action === 'show') {
+        $controller_object->show();
+    }elseif ($action === 'index') {
+        $controller_object->index();
     }
---
+++ src/controllers/home.php
@@ -0,0 +1,8 @@
+<?php 
+
+class Home {
+    public function index(): void
+    {
+        require "src/views/home_index.php";
+    }
+}

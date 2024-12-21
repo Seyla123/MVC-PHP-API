33d63fdfd14f1a356133f3dae942c8f7751e9185
Seav Seyla <Mrrseyla.758@gmail.com>
2024-12-21 09:24:20 +0700

    init

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php 
+
+$dsn = "mysql:host=localhost;dbname=product_db;charset=utf8;port=3306";
+
+$pdo = new PDO($dsn, 'product_db_user', 'Seyla758@',[
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+]);
+
+$stmt = $pdo->query("SELECT * FROM product");
+$pruducts = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+print_r($pruducts);
+?>

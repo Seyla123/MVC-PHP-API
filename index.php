9bcd3d820d0d0b4094f6485d3c57f9874308fbba
Seav Seyla <Mrrseyla.758@gmail.com>
2024-12-21 10:26:26 +0700

    feat(controller-action-and-front-controller) :Remove Repetition by Dynamically Creating Objects and Running Methods

--- index.php
+++ index.php
@@ -3,15 +3,6 @@
     $action = $_GET["action"]??"index";
     $controller = $_GET["controller"]??"products";	
 
-    if($controller === 'products') {
-        require "src/controllers/products.php";
-        $controller_object = new Products();
-    }elseif ($controller === "home") {
-        require "src/controllers/home.php";
-        $controller_object = new Home();
-    }
-    if($action === 'show') {
-        $controller_object->show();
-    }elseif ($action === 'index') {
-        $controller_object->index();
-    }
+    require "src/controllers/$controller.php";
+    $controller_object = new $controller;
+    $controller_object->$action();

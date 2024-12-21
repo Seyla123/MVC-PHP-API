9f8489e5fc9a8f768f3d34b0b0630ebd6208a672
Seav Seyla <Mrrseyla.758@gmail.com>
2024-12-21 09:39:52 +0700

    feat(mvc-basic) : create model file

--- index.php
+++ index.php
@@ -1,15 +1,7 @@
 <?php 
-
-$dsn = "mysql:host=localhost;dbname=product_db;charset=utf8;port=3306";
-
-$pdo = new PDO($dsn, 'product_db_user', 'Seyla758@',[
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-]);
-
-$stmt = $pdo->query("SELECT * FROM product");
-$products = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-print_r($products);
+require_once __DIR__.'/model.php';
+$model = new Model();
+$products = $model->getData();
 ?>
 <!DOCTYPE html>
 <html lang="en">
---
+++ model.php
@@ -0,0 +1,17 @@
+<?php
+
+class Model
+{
+    public function getData(): array
+    {
+        $dsn = "mysql:host=localhost;dbname=product_db;charset=utf8;port=3306";
+
+        $pdo = new PDO($dsn, 'product_db_user', 'Seyla758@', [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ]);
+
+        $stmt = $pdo->query("SELECT * FROM product");
+        $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $products;
+    }
+}

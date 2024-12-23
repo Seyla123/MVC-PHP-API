c22d8b85b294425f2ce2ba0465785f7fe6fc9334
Seav Seyla <Mrrseyla.758@gmail.com>
2024-12-23 11:03:22 +0700

    feat(class-organizetion):The Root Namespace and Relativity: Load the Model in its Namespace

--- src/App/controllers/Products.php
+++ src/App/controllers/Products.php
@@ -4,8 +4,7 @@ class Products
 {
     public function index(): void
     {
-        require "src/models/product.php";
-        $model = new Product();
+        $model = new \App\Models\Product;
         $products = $model->getData();
         require "src/views/products_index.php";
     }

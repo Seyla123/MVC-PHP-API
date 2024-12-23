795afc05baf28de1dee6b31725549ae1f13df082
Seav Seyla <Mrrseyla.758@gmail.com>
2024-12-23 09:26:07 +0700

    refact : Remove the Query String and Get the Controller and Action from the URL Path

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php 
 
-    $action = $_GET["action"]??"index";
-    $controller = $_GET["controller"]??"products";	
+    $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+   
+    $segments = explode("/", $path);
 
+    $action = $segments[2];
+    $controller = $segments[1];
     require "src/controllers/$controller.php";
     $controller_object = new $controller;
     $controller_object->$action();

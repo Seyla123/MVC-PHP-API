db7c5a7967c1c25417bac1168e14a64fa3d7c7d8
Seav Seyla <Mrrseyla.758@gmail.com>
2024-12-24 11:55:11 +0700

    feat(managing-dependencies): Validate the Type Declarations in the Service Container

--- src/Framework/Container.php
+++ src/Framework/Container.php
@@ -3,6 +3,7 @@ namespace Framework;
 
 use Closure;
 use ReflectionClass;
+use ReflectionNamedType;
 
 class Container
 {
@@ -24,6 +25,14 @@ class Container
         }
         foreach($contructor->getParameters() as $parameter){
             $type = $parameter->getType();
+            if($type === null){
+                exit("Contructor parameter '{$parameter->getName()}' in the $class_name class has no type declaration.");
+            }
+
+            if(!$type instanceof ReflectionNamedType){
+                exit("Contructor parameter '{$parameter->getName()}' in the $class_name class has invalid type: $type - only single named types supported.");
+            }
+
             if($type->isBuiltin()){
                 exit("Unable to resolve contructor parameters
                 '{$parameter->getName()}'

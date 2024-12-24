d08d94d35d2d4dac58ca9b3a7aa193b994dcf084
Seav Seyla <Mrrseyla.758@gmail.com>
2024-12-24 14:04:52 +0700

    feat(error-handling):Throw an Exception for a Malformed URL

--- index.php
+++ index.php
@@ -4,6 +4,11 @@
     
     $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
+    if($path === false){
+        throw new UnexpectedValueException("Malformed URL:
+                                            '{$_SERVER['REQUEST_URI']}'");
+    }
+
     spl_autoload_register(function ($class_name){
         require "src/" . str_replace("\\", "/", $class_name). ".php";
     });

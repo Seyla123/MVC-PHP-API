1d82933eeb1feb4b270152d29a9a93df9748fdd4
Seav Seyla <Mrrseyla.758@gmail.com>
2024-12-24 10:51:23 +0700

    feat(dependency-injection): Inject the Viewer into the Controller

--- src/App/controllers/Products.php
+++ src/App/controllers/Products.php
@@ -5,26 +5,25 @@ use Framework\Viewer;
 
 class Products 
 {
+    public function __construct(private Viewer $viewer){}
     public function index(): void
     {
         $model = new Product;
         $products = $model->getData();
 
-        $viewer = new Viewer;
-        echo $viewer->render("shared/header.php",[
+        echo $this->viewer->render("shared/header.php",[
             "title" => "Products"
         ]);
-        echo $viewer->render("Products/index.php", [
+        echo $this->viewer->render("Products/index.php", [
             "products"=> $products
         ]);
     }
     public function show(string $id): void
     {
-        $viewer = new Viewer;
-        echo $viewer->render("shared/header.php",[
+        echo $this->viewer->render("shared/header.php",[
             "title" => "Products"
         ]);
-        echo $viewer->render("Products/show.php", [
+        echo $this->viewer->render("Products/show.php", [
             "id"=> $id
         ]);
     }

2bab08ea698d0561d3c615f7628d14956f2b804d
Seav Seyla <Mrrseyla.758@gmail.com>
2024-12-24 17:05:00 +0700

    refactor(font-controller): Move the Routes to a Separate Configuration File

--- index.php
+++ index.php
@@ -17,19 +17,8 @@
                                             '{$_SERVER['REQUEST_URI']}'");
     }
 
+    $router = require "config/routes.php";
 
-
-    $router = new Framework\Router;
-
-    $router->add("/admin/{controller}/{action}", ["namespace"=> "Admin"]);
-    $router->add("/{title}/{id:\d+}/{page:\d+}", ["controller" => "products", "action" => "showPage"]);
-    $router->add("/product/{slug:[\w-]+}", ["controller" => "products", "action" => "show"]);
-    $router->add("/{controller}/{id:\d+}/{action}");
-    $router->add("/home/index", ["controller" => "home", "action" => "index"]);
-    $router->add("/products", ["controller" => "products", "action" => "index"]);
-    $router->add("/", ["controller" => "home", "action" => "index"]);
-    $router->add("/{controller}/{action}");
-    
     $container  = new Framework\Container;
 
     $container->set(App\Database::class, function(){

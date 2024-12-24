2c1ff53aaaea1fe2a9795c817a3c4800d8321d35
Seav Seyla <Mrrseyla.758@gmail.com>
2024-12-24 11:09:38 +0700

    feat(dependency-injection): Extract the Database Connection Code out into a Separate Class

--- src/App/Models/Product.php
+++ src/App/Models/Product.php
@@ -1,16 +1,13 @@
 <?php
 namespace App\Models;
 use PDO;
+use App\Database;
 class Product
 {
     public function getData(): array
     {
-        $dsn = "mysql:host=localhost;dbname=product_db;charset=utf8;port=3306";
-
-        $pdo = new PDO($dsn, 'product_db_user', 'Seyla758@', [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-        ]);
-
+        $database = new Database();
+        $pdo = $database->getConnection();
         $stmt = $pdo->query("SELECT * FROM product");
         $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
         return $products;

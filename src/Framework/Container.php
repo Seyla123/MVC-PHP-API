4b2fb15958b78aa758af6cead1c8b014854f01ae
Seav Seyla <Mrrseyla.758@gmail.com>
2024-12-24 13:57:27 +0700

    chore(error-handling) :Use Standard PHP Library (SPL) Exception Classes to Give More Detail

--- src/Framework/Container.php
+++ src/Framework/Container.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 namespace Framework;
 
 use Closure;
-use Exception;
+use InvalidArgumentException;
 use ReflectionClass;
 use ReflectionNamedType;
 
@@ -28,15 +28,15 @@ class Container
         foreach($contructor->getParameters() as $parameter){
             $type = $parameter->getType();
             if($type === null){
-                throw new Exception("Contructor parameter '{$parameter->getName()}' in the $class_name class has no type declaration.");
+                throw new InvalidArgumentException("Contructor parameter '{$parameter->getName()}' in the $class_name class has no type declaration.");
             }
 
             if(!$type instanceof ReflectionNamedType){
-                throw new Exception("Contructor parameter '{$parameter->getName()}' in the $class_name class has invalid type: $type - only single named types supported.");
+                throw new InvalidArgumentException("Contructor parameter '{$parameter->getName()}' in the $class_name class has invalid type: $type - only single named types supported.");
             }
 
             if($type->isBuiltin()){
-                throw new Exception("Unable to resolve contructor parameters
+                throw new InvalidArgumentException("Unable to resolve contructor parameters
                 '{$parameter->getName()}'
                 of type '{$type}' in the $class_name class.");
             }

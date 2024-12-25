54daa6c4c4dcf9138bda5944790de07196b888c6
Seav Seyla <Mrrseyla.758@gmail.com>
2024-12-25 14:50:20 +0700

    feat(model-updating) : Display the Form Containing the Record's Existing Attributes

--- src/views/Products/new.php
+++ src/views/Products/new.php
@@ -1,15 +1,9 @@
 <h1>New Product</h1>
 
 <form method="post" action="/products/create">
-    <label for="name">Name</label>
-    <input type="text" name="name" id="name">
-    <?php if(isset($errors["name"])): ?>
-        <p><?= $errors["name"] ?></p>
-    <?php endif; ?>
-    <label for="description">Description</label>
-    <textarea name="description" id="description"></textarea>
 
-    <button>Save</button>
+<?php require "form.php"; ?>
+
 </form>
 </body>
 </html>

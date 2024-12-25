6f28715616200c3004a7dcbaabfb04037fcd4002
Seav Seyla <Mrrseyla.758@gmail.com>
2024-12-25 14:05:56 +0700

    feat(model-inserting):  Add Specific Validation Error Messages for Each Field

--- src/App/Models/Product.php
+++ src/App/Models/Product.php
@@ -6,12 +6,22 @@ use Framework\Model;
 class Product extends Model
 {
    // protected $table = "product";
+   protected array $errors = [];
+
+   protected function addError(string $field, string $message): void
+   {
+      $this->errors[$field] = $message;
+   }
 
    protected function validate(array $data): bool 
    {
       if(empty($data["name"])) {
-         return false;
+         $this->addError("name", "Name is required");
       }
-      return true;
+      return empty($this->errors);
+   }
+   public function getErrors(): array
+   {
+      return $this->errors;
    }
 }

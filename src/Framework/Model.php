63ac996408ae2c1f0547d42815298a02dd605745
Seav Seyla <Mrrseyla.758@gmail.com>
2024-12-25 13:29:03 +0700

    feat(model): Default the Table Name to the Lowercase Model Class Name

--- src/Framework/Model.php
+++ src/Framework/Model.php
@@ -6,14 +6,20 @@ use App\Database;
 abstract class Model
 {
     protected $table;
+    private function getTable():string
+    {
+        if($this->table !== null) return $this->table;
+        
+        $parts = explode("\\", $this::class);
+        return strtolower(array_pop($parts));
+    }
     public function __construct(private Database $database)
     {
-        
     }
     public function findAll(): array
     {
         $pdo = $this->database->getConnection();
-        $sql = "SELECT * FROM {$this->table}";
+        $sql = "SELECT * FROM {$this->getTable()}";
         $stmt = $pdo->query($sql);
         $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
         return $products;
@@ -21,7 +27,7 @@ abstract class Model
     public function find(string $id): array|bool
     {
         $conn = $this->database->getConnection();
-        $stmt = $conn->prepare("SELECT * FROM {$this->table} WHERE id = :id");
+        $stmt = $conn->prepare("SELECT * FROM {$this->getTable()} WHERE id = :id");
         $stmt->bindValue(":id", $id, PDO::PARAM_INT);
         $stmt->execute();
         return $stmt->fetch(PDO::FETCH_ASSOC);

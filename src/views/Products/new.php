a087d698c6099ab618f8c0f418cb119b6fc4a2b0
Seav Seyla <Mrrseyla.758@gmail.com>
2024-12-25 13:38:40 +0700

    feat(product-new): Add a Form for Creating a New Record

---
+++ src/views/Products/new.php
@@ -0,0 +1,11 @@
+<h1>New Product</h1>
+
+<form action="post">
+    <label for="name">Name</label>
+    <input type="text" name="name" id="name">
+
+    <label for="description">Description</label>
+    <textarea name="description" id="description"></textarea>
+
+    <button>Save</button>
+</form>

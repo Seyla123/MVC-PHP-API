075589713bf3adcbc339c0c03f0737496ec1f7c4
Seav Seyla <Mrrseyla.758@gmail.com>
2024-12-26 10:47:08 +0700

    feat(variables-and-control-structure): Replace PHP Variables with the Alternative Variable Syntax

--- src/Framework/MVCTemplateViewer.php
+++ src/Framework/MVCTemplateViewer.php
@@ -5,9 +5,16 @@ class MVCTemplateViewer implements TemplateViewerInterface
 {
     public function render(string $template, array $data=[]): string
     {
-        extract($data, EXTR_SKIP);
-        ob_start();
-        require dirname(__DIR__) . "/views/$template";
-        return ob_get_clean();
+        $code = file_get_contents(dirname(__DIR__) . "/views/$template");
+        $code = $this->replaceVariables($code);
+        return $code;
+        // extract($data, EXTR_SKIP);
+        // ob_start();
+        // require dirname(__DIR__) . "/views/$template";
+        // return ob_get_clean();
+    }
+    private function replaceVariables(string $code): string
+    {
+        return preg_replace("#{{\s*(\S+)\s*}}#", "<?= htmlspecialchars(\$$1) ?>", $code);
     }
 }
--- src/views/Products/index.mvc.php
+++ src/views/Products/index.mvc.php
@@ -1,19 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= $title ?></title>
+    <title>{{ title }}</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
 </head>
+
 <body>
     <h1>Products</h1>
     <a href="/products/new">New Product</a>
-    <p><?= $total ?></p>
+    <p>{{ total }}</p>
     <?php foreach ($products as $product): ?>
         <h2>
-            <a href="/products/<?= $product['id'] ?>/show"><?=htmlspecialchars($product['name'])?></a>
+            <a href="/products/{{product['id']}}/show">{{ product['name'] }}</a>
         </h2>
     <?php endforeach; ?>
 </body>
+
 </html>

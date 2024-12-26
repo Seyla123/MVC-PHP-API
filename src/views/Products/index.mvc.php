8560b42ff867b56dc5eece2feca97a2819c7a766
Seav Seyla <Mrrseyla.758@gmail.com>
2024-12-26 11:08:17 +0700

    feat(template-inheritance): Create a Base Template and Modify the Index Template to Extend It

--- src/views/Products/index.mvc.php
+++ src/views/Products/index.mvc.php
@@ -1,14 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
+{% extends "base.mvc.php" %}
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ title }}</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
-</head>
+{% block title %}Products{% endblock %}
 
-<body>
+{% block body %}
     <h1>Products</h1>
     <a href="/products/new">New Product</a>
     <p>{{ total }}</p>
@@ -17,6 +11,4 @@
             <a href="/products/{{product['id']}}/show">{{ product['name'] }}</a>
         </h2>
     {% endforeach; %}
-</body>
-
-</html>
+{% endblock %}

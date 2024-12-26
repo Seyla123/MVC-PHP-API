997a1c87775ee1fa65466189c9c7076a68c0453a
Seav Seyla <Mrrseyla.758@gmail.com>
2024-12-26 16:11:12 +0700

    feat(middleware): Add a Middleware Class that Returns a Redirect Response

--- config/middleware.php
+++ config/middleware.php
@@ -2,5 +2,6 @@
 
 return [
     "message"=>\App\Middleware\ChangeResponseExample::class,
-    "trim"=>\App\Middleware\ChangeRequestExample::class
+    "trim"=>\App\Middleware\ChangeRequestExample::class,
+    "deny"=>\App\Middleware\RedirectExample::class
 ];
